7b6b583cfd6200228885835eceb6324e06103531
moon <rycmoon@gmail.com>
2022-05-27 17:20:18 -0500

    version one of the signup form

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import {
-  Grid,
-  Box,
-  Typography,
-  Button,
-  FormControl,
-  TextField,
-  FormHelperText,
-} from '@material-ui/core';
+import React, {useEffect, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import {Box, FormHelperText, useMediaQuery,} from '@material-ui/core';
+import {loginSignupStyles} from "./GlobalStyles";
+import SideBanner from "./components/SideBanner";
+import LoginSignupNav from "./components/LoginSignupNav";
+import SignupForm from "./components/SignupForm";
 
 const Signup = ({ user, register }) => {
   const history = useHistory();
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down('xs'));
+  const classes = loginSignupStyles({isMobile})
 
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -36,75 +34,96 @@ const Signup = ({ user, register }) => {
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Link href="/login" to="/login">
-            <Button>Login</Button>
-          </Link>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
+      <Box className={classes.root}>
+
+        {/*#region Side Banner and mobile view*/}
+        <SideBanner isMobile={isMobile}>
+          <SignupForm handleRegister={handleRegister} isMobile={isMobile}/>
+        </SideBanner>
+        {/*#endregion*/}
+
+        {!isMobile &&
+            <Box sx={{display: 'flex', flexDirection: 'column', width: '-webkit-fill-available'}}>
+              {/*Login to existing Account*/}
+              <LoginSignupNav isMobile={isMobile}/>
+              {/*Signup Form*/}
+              <SignupForm handleRegister={handleRegister} isMobile={isMobile}/>
+            </Box>
+        }
+        <FormHelperText>
+          {formErrorMessage.confirmPassword}
+        </FormHelperText>
+
       </Box>
-    </Grid>
+    // <Grid container justifyContent="center">
+    //   <Box>
+    //     <Grid container item>
+    //       <Typography>Need to log in?</Typography>
+    //       <Link href="/login" to="/login">
+    //         <Button>Login</Button>
+    //       </Link>
+    //     </Grid>
+    //     <form onSubmit={handleRegister}>
+    //       <Grid>
+    //         <Grid>
+    //           <FormControl>
+    //             <TextField
+    //               aria-label="username"
+    //               label="Username"
+    //               name="username"
+    //               type="text"
+    //               required
+    //             />
+    //           </FormControl>
+    //         </Grid>
+    //         <Grid>
+    //           <FormControl>
+    //             <TextField
+    //               label="E-mail address"
+    //               aria-label="e-mail address"
+    //               type="email"
+    //               name="email"
+    //               required
+    //             />
+    //           </FormControl>
+    //         </Grid>
+    //         <Grid>
+    //           <FormControl error={!!formErrorMessage.confirmPassword}>
+    //             <TextField
+    //               aria-label="password"
+    //               label="Password"
+    //               type="password"
+    //               inputProps={{ minLength: 6 }}
+    //               name="password"
+    //               required
+    //             />
+    //             <FormHelperText>
+    //               {formErrorMessage.confirmPassword}
+    //             </FormHelperText>
+    //           </FormControl>
+    //         </Grid>
+    //         <Grid>
+    //           <FormControl error={!!formErrorMessage.confirmPassword}>
+    //             <TextField
+    //               label="Confirm Password"
+    //               aria-label="confirm password"
+    //               type="password"
+    //               inputProps={{ minLength: 6 }}
+    //               name="confirmPassword"
+    //               required
+    //             />
+    //             <FormHelperText>
+    //               {formErrorMessage.confirmPassword}
+    //             </FormHelperText>
+    //           </FormControl>
+    //         </Grid>
+    //         <Button type="submit" variant="contained" size="large">
+    //           Create
+    //         </Button>
+    //       </Grid>
+    //     </form>
+    //   </Box>
+    // </Grid>
   );
 };
 
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import {loginSignupStyles} from "../GlobalStyles";
+import {Box, Button, FormControl, TextField, Typography} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 const SignupForm = (props) => {
+    const {handleRegister,isMobile} = props
+    const classes = loginSignupStyles(props)
     return (
-        <div>
+        <Box component={'form'} onSubmit={handleRegister} className={classes.formLoginRoot}>
+            <Box>
+                <Typography className={classes.formWelcomeText}>Create an account.</Typography>
 
-        </div>
+                {/*#region username*/}
+                <Typography className={classes.formInputText}>Username</Typography>
+                <FormControl required className={classes.formInput}>
+                    <TextField
+                        className={classes.formInput}
+                        aria-label={'username'}
+                        name={'username'} type={'text'}
+                    />
+                </FormControl>
+                {/*#endregion*/}
+
+                {/*region password*/}
+                <Typography className={`${classes.formInputText} ${classes.formInputTextMarginTop}`}>E-mail address</Typography>
+                <FormControl required className={classes.formInput}>
+                    <TextField className={classes.formInput} aria-label={'e-mail address'}
+                               name={'email'} type={'email'}/>
+                </FormControl>
+                {/*#endregion*/}
+
+                {/*region confirm password*/}
+                <Typography className={`${classes.formInputText} ${classes.formInputTextMarginTop}`}>Password</Typography>
+                <FormControl required className={classes.formInput}>
+                    <TextField className={classes.formInput} aria-label={'password'}
+                               name={'password'} type={'password'}
+                                inputProps={{minLength:6}}
+                    />
+                </FormControl>
+                {/*#endregion*/}
+
+                {/*#region Button*/}
+                <Box className={classes.formLoginButtonRoot}>
+                    <Button className={classes.formLoginButtonBtn} variant={'contained'} type={'submit'}>
+                        <Typography className={classes.formLoginButtonText}>Create</Typography>
+                    </Button>
+                    {isMobile &&
+                        <Link className={classes.createAccountLink} to={"/login"}>
+                            <Button variant={"contained"} className={classes.createAccountButtonBtn}>
+                                <Typography className={classes.createAccountButtonText}>Login</Typography>
+                            </Button>
+                        </Link>
+                    }
+                </Box>
+                {/*#endregion*/}
+
+            </Box>
+        </Box>
     );
 };
 
