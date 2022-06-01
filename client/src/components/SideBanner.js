5dd936e7afc05750ab193858041891bdfd016a52
Ryan Moon <rycmoon@gmail.com>
2022-06-01 16:02:39 -0500

    updated the styles for the side banner and now the side banner scales using the grid component

--- client/src/components/SideBanner.js
+++ client/src/components/SideBanner.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {Box, Typography} from "@material-ui/core";
+import {Box, Grid, Typography} from "@material-ui/core";
 import {loginSignupStyles} from "../GlobalStyles";
 
 const SideBanner = ({isMobile, children}) => {
     const classes = loginSignupStyles({isMobile})
 
     return (
-        <Box className={classes.sideBannerRoot}>
+        <Grid item className={classes.sideBannerRoot} xs={12} sm={5}>
             <Box className={classes.sideBannerBg}/>
             <Box className={classes.sideBannerTextRoot}>
                 <img alt={''} className={classes.sideBannerTextChat}
                      src={`${process.env.PUBLIC_URL}/assets/bubble.svg`}/>
-                {!isMobile && <Typography className={classes.sideBannerText}>Converse with anyone with any language</Typography>}
-                {isMobile && children }
+                {!isMobile &&
+                    <Typography className={classes.sideBannerText}>Converse with anyone with any language</Typography>}
+                {isMobile && children}
             </Box>
-        </Box>
+        </Grid>
     );
 };
 
